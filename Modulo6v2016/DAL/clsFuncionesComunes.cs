e8090110599a8f4563a05efb86e949149869e2af
SAUL <SAUL@SICH>
2016-12-05 22:41:56 -0600

    SICH: se agrega archivo de funciones comunes.

---
+++ Modulo6v2016/DAL/clsFuncionesComunes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Data.OleDb;
+
+//SICH:
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Modulo6
+{
+    public static class clsFuncionesComunes
+    {
+        public static string Get_ConfigValue(string Name)
+        {
+            return ConfigurationManager.AppSettings[Name] != null ? ConfigurationManager.AppSettings[Name].ToString() : string.Empty;
+        }
+
+        public static bool CargarDropDownList(ref ComboBox oDDL, string sQuery, string sValue, string sDisplay)
+        {
+            bool bResultado;
+            cConexion oConn = new cConexion(@"workstation id=SICH; Data Source=SICH; TRUSTED_CONNECTION=YES; persist security info=False; Initial Catalog=MODULO_6X; packet size=4096; ");
+            //"workstation id=SICH; data source=SICH; TRUSTED_CONNECTION=YES; persist security info=False; initial catalog=Axtel_Captura; packet size=4096; "
+            SqlCommand oCmd = new SqlCommand(sQuery);
+            oCmd.CommandType = CommandType.Text;
+
+            try
+            {
+                DataTable oDTDatos = oConn.Consultar(oCmd);
+
+                DataRow oDR = oDTDatos.NewRow();
+                oDR[sValue] = 0; //SICH 28ENE2016: se cambia de -1 de acuerdo al requerimiento de AXTEL
+                oDR[sDisplay] = "Seleccione una opción";
+                oDTDatos.Rows.InsertAt(oDR, 0);
+
+                oDDL.ValueMember = sValue;
+                oDDL.DisplayMember = sDisplay;
+                oDDL.DataSource = oDTDatos;
+                bResultado = true;
+            }
+            catch (Exception ex)
+            {
+                bResultado = false;
+            }
+            return bResultado;
+        }
+
+        public static void Cell_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!ValidarNumero(e.KeyChar))
+            {
+                e.Handled = true;
+                return;
+            }
+        }
+
+        public static bool ValidarNumero(char cCaracter)
+        {
+            if (!(char.IsNumber(cCaracter)) && (cCaracter != (char)Keys.Back) && (cCaracter != '.'))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static string fnMapRespuesta(int iOpcion) {
+            if (iOpcion<=0 && iOpcion > Convert.ToInt16(clsFuncionesComunes.Get_ConfigValue("MAX_NUM_OPCIONES")))
+            {return "@";}            
+
+            return Convert.ToString(Convert.ToChar(65 + iOpcion -1)).Trim();
+        }
+
+        public static int fnRevMapRespuesta(char cOpcion) {
+            return (Convert.ToInt16(cOpcion) - 65 + 1);
+        }
+    }
+}
